8e1c9155bbe160e56450eaae9c9e9a45b21e23c2
MalinduLiyanage <malindumadhubashana@gmail.com>
2025-02-26 13:57:29 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ContactComponent} from './views/contact/contact.component';
+import {AboutComponent} from './views/about/about.component';
+import {ServicesComponent} from './views/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+
+];
